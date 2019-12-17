0f78c93b09c452ebf68248b0a5718c39905157c6
Arpit Bhatia <arpit16229@iiitd.ac.in>
2019-12-17 23:20:43 +0530

    Test new site

---
+++ master.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+    $("body").fadeIn(250); 
+    $("a").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(250, redirectPage);      
+    });         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+});
